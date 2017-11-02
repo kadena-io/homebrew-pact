94d2356cdc4a1d9fe4858b20e5229a5ebfb8670d
Stuart Popejoy <spopejoy@panix.com>
2017-11-01 21:50:12 -0400

    Pact 2.3.1

--- pact.rb
+++ pact.rb
@@ -1,8 +1,8 @@
 class Pact < Formula
   desc "Interpreter for the Pact smart contract language"
   homepage "https://github.com/kadena-io/pact"
-  url "http://kadena.io/pact/builds/2.3.0/osx/pact-2.3.0.tar.gz"
-  sha256 "485a4790f7306f031b58040134656eea00c37ab649dfed9c76e5e0689526b482"
+  url "http://kadena.io/pact/builds/2.3.1/osx/pact-2.3.1.tar.gz"
+  sha256 "595710e06d439c1bd8cc1d3d35c54dfd2a730936eab457a60d31551da5eb9bde"
 
   bottle :unneeded
 
