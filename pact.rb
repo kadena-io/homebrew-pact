91787ae812f43ccaf18b80ffaf4d36ecb7d87b0e
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2019-02-13 15:17:37 -0500

    z3 doesn't support pins unfort

--- pact.rb
+++ pact.rb
@@ -4,7 +4,7 @@ class Pact < Formula
   url "http://kadena.io/pact/builds/2.6.1/osx/pact-2.6.1.tar.gz"
   sha256 "4670575f74dffd98d4803d1216367c7f974cbc9f27b36bcbb92a262f17724cd5"
 
-  depends_on "https://raw.githubusercontent.com/Homebrew/homebrew-core/a7e7806193f7605c7fef6110655911012d3f1eb3/Formula/z3.rb"
+  depends_on "z3"
 
   bottle :unneeded
 
