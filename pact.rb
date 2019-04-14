715412a78d89865434a29645e03aa3dea1a5e2a1
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2019-04-13 23:48:26 -0400

    move to install.kadena.io

--- pact.rb
+++ pact.rb
@@ -1,7 +1,7 @@
 class Pact < Formula
   desc "Interpreter for the Pact smart contract language"
   homepage "https://github.com/kadena-io/pact"
-  url "http://kadena.io/pact/builds/2.6.1/osx/pact-2.6.1.tar.gz"
+  url "http://install.kadena.io/pact/builds/2.6.1/osx/pact-2.6.1.tar.gz"
   sha256 "4670575f74dffd98d4803d1216367c7f974cbc9f27b36bcbb92a262f17724cd5"
 
   depends_on "z3"
