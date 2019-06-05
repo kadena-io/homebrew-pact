245f24d0f0b4224f794eebe49525c62e86b1cdf0
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2019-06-05 19:20:32 -0400

    fix url

--- pact.rb
+++ pact.rb
@@ -1,7 +1,7 @@
 class Pact < Formula
   desc "Interpreter for the Pact smart contract language"
   homepage "https://github.com/kadena-io/pact"
-  url "http://kadena.io/pact/builds/3.0/osx/pact-3.0.tar.gz"
+  url "http://install.kadena.io/pact/builds/3.0/osx/pact-3.0.tar.gz"
   sha256 "daa6ce68e37c73df6bad71daa811c5d2616414c249b790e270ec338d629cb388"
 
   depends_on "z3"
