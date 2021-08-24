0c1bfdca5fff004fa5dd28eb716629e04a1ebe04
Stuart Popejoy <slpopejoy@users.noreply.github.com>
2021-08-24 18:32:41 -0400

    try 5

--- z3.rb
+++ z3.rb
@@ -1,5 +1,5 @@
 class Z3 < Formula
-  desc "High-performance theorem prover, pact binary distro"
+  desc "High-performance theorem prover, pact binary distribution"
   homepage "https://github.com/Z3Prover/z3"
   url "https://github.com/kadena-io/pact/releases/download/v4.1/z3-4.8.10-osx.tar.gz"
   sha256 "8db49ec89614708874c68f7b72e7704108beebbb416780f5e09db18f6cb032eb"
