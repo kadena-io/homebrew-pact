1a93cf56f76b71f815ac66806b3bc700f18bb4c4
Emily Pillmore <emilypi@cohomolo.gy>
2022-01-19 09:21:19 -0700

    update homebrew

--- pact.rb
+++ pact.rb
@@ -1,9 +1,8 @@
 class Pact < Formula
   desc "Interpreter for the Pact smart contract language"
   homepage "https://github.com/kadena-io/pact"
-  url "https://github.com/kadena-io/pact/releases/download/v4.1.1/pact-4.1.1-osx.tar.gz"
-  sha256 "fedd8db8715df6b9c18c8fec6bb01a864907367ac980eb5546899f4790478db7"
-
+  url "https://github.com/kadena-io/pact/releases/download/v4.1.1/pact-4.2.0-osx.tar.gz"
+  sha256 "0d41ef837cd7f097106ca04dc9c4067db81dff462dc89a70c7257d34290a39dd"
   depends_on "kadena-io/pact/z3"
 
   bottle :unneeded
