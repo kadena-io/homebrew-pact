de53282716c9ebae039199e482e501a725468247
Jose <cardona.josem@gmail.com>
2022-09-07 12:32:18 -0400

    pact 4.4 release

--- pact.rb
+++ pact.rb
@@ -1,8 +1,8 @@
 class Pact < Formula
   desc "Interpreter for the Pact smart contract language"
   homepage "https://github.com/kadena-io/pact"
-  url "https://github.com/kadena-io/pact/releases/download/v4.3.1/pact-4.3.1-osx.tar.gz"
-  sha256 "162f7ab4380a58db120a08b70beb770f024ed35ad541e77c022e2c2d98e47578"
+  url "https://github.com/kadena-io/pact/releases/download/v4.4/pact-4.4-osx.tar.gz"
+  sha256 "4ec697dcd800aede26adedac0a1c8580e460f5f6a9d71d727ba562f83be5dd07"
   depends_on "kadena-io/pact/z3"
 
   def install
