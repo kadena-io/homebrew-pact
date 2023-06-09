35c0a37631e7c12fc4cbd65745be64dee1601852
Emily Pillmore <emilypi@cohomolo.gy>
2023-06-09 17:29:11 -0600

    bump pact-4.7.1

--- pact.rb
+++ pact.rb
@@ -1,8 +1,8 @@
 class Pact < Formula
   desc "Interpreter for the Pact smart contract language"
   homepage "https://github.com/kadena-io/pact"
-  url "https://github.com/kadena-io/pact/releases/download/v4.7.0/pact-4.7.0-osx.tar.gz"
-  sha256 "76fb46c2d25f3a589212dcd4086b170e203bce969f714bbba48bc76a084c9896"
+  url "https://github.com/kadena-io/pact/releases/download/v4.7.1/pact-4.7.1-osx.tar.gz"
+  sha256 "612b8b7133ae53def442b67239e3d192c9a9f081a8339a3eae80c2aa5ae71287"
   depends_on "kadena-io/pact/z3"
 
   def install
