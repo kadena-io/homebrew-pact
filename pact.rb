bca4c53fe17ec6752671bd5d10431263c547fb8e
Emily Pillmore <emily@kadena.io>
2023-08-22 13:36:35 -0600

    update pact version to 4.8

--- pact.rb
+++ pact.rb
@@ -1,7 +1,7 @@
 class Pact < Formula
   desc "Interpreter for the Pact smart contract language"
   homepage "https://github.com/kadena-io/pact"
-  url "https://github.com/kadena-io/pact/releases/download/v4.8.0/pact-4.8.0-osx.tar.gz"
+  url "https://github.com/kadena-io/pact/releases/download/v4.8/pact-4.8-osx.tar.gz"
   sha256 "bbe08a28487af96c27d0c59d4f5ebfac766e74f2cd424dba5d3659eb8d8b98a8"
   depends_on "kadena-io/pact/z3"
 
