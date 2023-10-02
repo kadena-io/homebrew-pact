52ecda8352b1245441cd8be234585134a1d1130a
Emily Pillmore <emilypi@cohomolo.gy>
2023-10-02 14:14:20 -0600

    pact 4.9.0

--- pact.rb
+++ pact.rb
@@ -1,8 +1,8 @@
 class Pact < Formula
   desc "Interpreter for the Pact smart contract language"
   homepage "https://github.com/kadena-io/pact"
-  url "https://github.com/kadena-io/pact/releases/download/v4.8/pact-4.8-osx.tar.gz"
-  sha256 "bbe08a28487af96c27d0c59d4f5ebfac766e74f2cd424dba5d3659eb8d8b98a8"
+  url "https://github.com/kadena-io/pact/releases/download/v4.9.0/pact-4.9.0-osx.tar.gz"
+  sha256 "2999417fcef527cb89fc3bbbacfe6e0c23f39b2b106f77d2d369a9b783096574"
   depends_on "kadena-io/pact/z3"
 
   def install
