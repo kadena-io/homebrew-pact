5580155a01aac6df68db18d1d2b89aca72ef7fe9
jmcardon <cardona.josem@gmail.com>
2024-04-10 16:06:17 -0400

    remove z3 from brew formula

--- pact.rb
+++ pact.rb
@@ -3,7 +3,13 @@ class Pact < Formula
   homepage "https://github.com/kadena-io/pact"
   url "https://github.com/kadena-io/pact/releases/download/v4.11.0/pact-4.11.0-osx.tar.gz"
   sha256 "8d3e194a13593cdf33dcd314cb0ca2452302106bfe238624f8e9761c9a4e1ca2"
-  depends_on "kadena-io/pact/z3"
+  depends_on "z3"
+  depends_on "openssl@3"
+
+  on_arm do
+    url "https://github.com/kadena-io/pact/releases/download/v4.11.0/pact-4.11.0-aarch64-osx.tar.gz"
+    sha256 "4bdd64fe18e20bf1582a5aeea58852635ec2f626cf672e48e00e40a05d3ee3ff"
+  end
 
   def install
     bin.install "pact"
