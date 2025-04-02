4d9fb9e98552339712f503f30be496ede4cde03b
Kadena DevOps <devops@kadena.io>
2025-04-02 18:39:43 +0000

    pact package version update

--- pact.rb
+++ pact.rb
@@ -16,7 +16,7 @@ class Pact < Formula
     head do
       on_arm do
         url "https://github.com/kadena-io/pact-5/releases/download/nightly/pact-nightly-darwin-aarch64.tar.gz"
-      sha256 "5646fa545b85d5e4fdc20d89b5fe42272b7dac11833c90521ebd9f6499ecd244"
+      sha256 "4dbfdb946350505d14d1e92d8b2cdebe2fcd539fd5501be8a788df6df42a39e7"
       end
     end
 
